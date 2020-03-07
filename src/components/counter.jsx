83bbd826e838e7d5aea88c081b8c1fe14c16f67c
Majd Rezik <majdrezik@gmail.com>
2020-03-07 03:33:06 +0200

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,21 +1,6 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
-  };
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
-
-  styles = {
-    fontSize: 15,
-    fontWeight: "bold"
-  };
-
   /*event handler, this keyword isn't defined here
     so we either use the bind method in the constructor or
     we use the arrow function (arrow functions are inhereted, they can use this keyword)
@@ -27,42 +12,56 @@ class Counter extends Component {
     }
     
   */
-  handleIncrement = () => {
-    this.state.count++;
-    console.log("Increment clicked!");
+
+  // renderTags() {
+  //   if (this.props.Counter.length === 0) return "There are no tags!";
+  //   return (
+  //     <ul>
+  //       {this.state.tags.map(tag => (
+  //         <li key={tag}> {tag} </li>
+  //       ))}
+  //     </ul>
+  //   );
+  // }
+
+  styles = {
+    fontSize: 15,
+    fontWeight: "bold"
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return "There are no tags!";
-    return (
-      <ul>
-        {this.state.tags.map(tag => (
-          <li key={tag}> {tag} </li>
-        ))}
-      </ul>
-    );
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 
   render() {
     return (
       <div>
+        {/* {console.log(this.props)} */}
+        {this.props.children}
         <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button
           className="btn btn-secondary btn-sm"
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
         >
           Increment
         </button>
-        {this.renderTags()}
+        {/* {this.renderTags()} */}
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
+        </button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 }
